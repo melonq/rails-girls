880e5b1baffc33c06b2a8e8fcb2aea330c631092
Xiang Zhang <xmzhang@thoughtworks.com>
2015-09-08 20:39:08 +0800

    第一步：建立一个rails项目

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Rails Girls App
 详细介绍请见[这篇文档](http://guides.railsgirls.com/app/)
+
+# 第一步：建立一个rails应用
+## 命令解析
+`mkdir`是建立一个文件夹，后面跟一个字符串当文件夹名
+
+`cd`是进入某一个文件夹中，可以用相对路径，也可以用绝对路径
+
+`pwd`可以查看当前目录的绝对路径
+
+`rails new`命令可以帮助你快速建立一个rails项目
+
+`rails server`用来启动当前目录下的rails项目
+
+## 这一步所需操作
+```
+mkdir projects
+cd projects
+rails new railsgirls
+cd railsgirls
+rails server
+```
+完成这些步骤后，注意看命令行的提示信息，访问打印出来的URL就可以打开你的第一个rails应用啦
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = 'b59dd9fb8900333d304c4302d9e898a7fd03bed31c8da2702d9d811e32728b6ea488ddd10ca9e239af59ee5177ca0ee7f9d05c2c6dadd80cc8c932d8d86e323e'
